0f355a37a48e96b5bc7725f0a19b27cc795164cf
Jafar Abdi <cafer.abdi@gmail.com>
2020-01-08 02:03:40 +0300

    Remove sudo

--- scripts/reprepro-include.bash
+++ scripts/reprepro-include.bash
@@ -17,11 +17,11 @@ export DISTRO=${3}
 export ARCH=${4}
 
 # invalidate dependent
-sudo reprepro -V -b $REPO_DIR removefilter $DISTRO "Package (% * ), Architecture (==$ARCH), (Depends (% *$PKG[, ]* ) | Depends (% *$PKG ) )"
+reprepro -V -b $REPO_DIR removefilter $DISTRO "Package (% * ), Architecture (==$ARCH), (Depends (% *$PKG[, ]* ) | Depends (% *$PKG ) )"
 
 # invalidate this package
-sudo reprepro -V -b $REPO_DIR removefilter $DISTRO "Package (==$PKG), Architecture (==$ARCH)"
+reprepro -V -b $REPO_DIR removefilter $DISTRO "Package (==$PKG), Architecture (==$ARCH)"
 
-sudo reprepro -V -b $REPO_DIR deleteunreferenced
+reprepro -V -b $REPO_DIR deleteunreferenced
 
-sudo reprepro -V -b $REPO_DIR includedeb $DISTRO $BUILD_DIR/binarydebs/$NAME
+reprepro -V -b $REPO_DIR includedeb $DISTRO $BUILD_DIR/binarydebs/$NAME
